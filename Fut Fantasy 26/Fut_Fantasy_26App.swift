5ae6f5944f529a7b526d25fee105faf86c7ec3b0
jose julian Lopez Huacuja <jose.lopez4636@alumnos.udg.mx>
2025-10-12 00:08:03 -0600

    Initial Commit

---
+++ Fut Fantasy 26/Fut_Fantasy_26App.swift
@@ -0,0 +1,17 @@
+//
+//  Fut_Fantasy_26App.swift
+//  Fut Fantasy 26
+//
+//  Created by Jose julian Lopez on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Fut_Fantasy_26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
